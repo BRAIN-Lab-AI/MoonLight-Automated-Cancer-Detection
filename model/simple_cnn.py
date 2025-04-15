24fc51fd0a388e397767201b28fb1344b9bca0e9
hsn6647 <h.j.aldahneen@gmail.com>
2025-04-16 01:57:02 +0300

    Add files via upload

--- model/simple_cnn.py
+++ model/simple_cnn.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 class SimpleCNN(nn.Module):
     def __init__(self, num_classes=2):
-        super(DefaultModel, self).__init__()
+        super(SimpleCNN, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 32, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
             nn.Conv2d(32, 64, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
